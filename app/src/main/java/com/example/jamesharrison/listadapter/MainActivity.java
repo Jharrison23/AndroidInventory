74b943a3e3468d3aef98b7b7d1ba98ba37dc4116
jharrison23 <Jh0698018848>
2016-12-03 17:51:34 -0500

    Coded the mainactivity by initializing all the UI elements and also adding the code for the view and add button

--- app/src/main/java/com/example/jamesharrison/listadapter/MainActivity.java
+++ app/src/main/java/com/example/jamesharrison/listadapter/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.jamesharrison.listadapter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 // Project by James Harrison, example to model and inventory which reads and writes to an sqlite database
 public class MainActivity extends AppCompatActivity
@@ -30,7 +33,60 @@ public class MainActivity extends AppCompatActivity
         btnAdd = (Button) findViewById(R.id.btnAdd);
         btnView = (Button) findViewById(R.id.btnView);
 
-        //myDB = new DatabaseHelper(this);
+        myDB = new DatabaseHelper(this);
+
+        btnView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, ViewListContents.class);
+
+                startActivity(intent);
+
+            }
+        });
+
+        btnAdd.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+                String name = itemName.getText().toString();
+                String description = itemDescription.getText().toString();
+                String price =  itemPrice.getText().toString();
+                String quantity = itemQuantity.getText().toString();
+
+                if(name.length() != 0 && description.length() != 0 && price.length() != 0 && quantity.length() != 0)
+                {
+                    // try to add the elements to the database
+                    boolean result = myDB.addData(name, description, price, quantity);
+
+                    if (result)
+                    {
+                        Toast.makeText(MainActivity.this, "Item Added to Inventory", Toast.LENGTH_SHORT).show();
+                    }
+
+                    else
+                    {
+                        Toast.makeText(MainActivity.this, "Something wrong cant add", Toast.LENGTH_SHORT).show();
+                    }
+
+                    // once everything is added reset the UI elements to make the blank again
+                    itemName.setText("");
+                    itemPrice.setText("");
+                    itemDescription.setText("");
+                    itemQuantity.setText("");
+
+                }
+
+                else
+                {
+                    Toast.makeText(MainActivity.this, "There is nothing to add", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
 
---
+++ app/src/main/java/com/example/jamesharrison/listadapter/ViewListContents.java
@@ -0,0 +1,8 @@
+package com.example.jamesharrison.listadapter;
+
+/**
+ * Created by jamesharrison on 12/3/16.
+ */
+
+public class ViewListContents {
+}
