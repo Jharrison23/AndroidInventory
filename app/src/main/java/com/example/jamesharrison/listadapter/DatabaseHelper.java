2783b8ed61fd24be14a3530020237e8d7a67856f
jharrison23 <Jh0698018848>
2016-12-03 16:50:41 -0500

    small commit to initialize all ui elements

---
+++ app/src/main/java/com/example/jamesharrison/listadapter/DatabaseHelper.java
@@ -0,0 +1,10 @@
+package com.example.jamesharrison.listadapter;
+
+/**
+ * Created by jamesharrison on 12/3/16.
+ */
+
+public class DatabaseHelper
+{
+
+}
--- app/src/main/java/com/example/jamesharrison/listadapter/MainActivity.java
+++ app/src/main/java/com/example/jamesharrison/listadapter/MainActivity.java
@@ -2,15 +2,38 @@ package com.example.jamesharrison.listadapter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 // Project by James Harrison, example to model and inventory which reads and writes to an sqlite database
 public class MainActivity extends AppCompatActivity
 {
+
+    DatabaseHelper myDB;
+
+    EditText itemName, itemPrice, itemQuantity, itemDescription;
+    Button btnAdd, btnView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        itemName = (EditText) findViewById(R.id.itemName);
+        itemPrice = (EditText) findViewById(R.id.itemPrice);
+        itemDescription = (EditText) findViewById(R.id.itemDescription);
+        itemQuantity = (EditText) findViewById(R.id.itemQuantity);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnView = (Button) findViewById(R.id.btnView);
+
+        //myDB = new DatabaseHelper(this);
+
+
+
+
     }
 }
