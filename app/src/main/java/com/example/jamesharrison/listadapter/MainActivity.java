2aebad0369072b58c1001655d8b891e5821a5347
jharrison23 <Jh0698018848>
2016-12-03 16:42:12 -0500

    initial commit

---
+++ app/src/main/java/com/example/jamesharrison/listadapter/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jamesharrison.listadapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// Project by James Harrison, example to model and inventory which reads and writes to an sqlite database
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
